4c3345ddf5d24084e956fafcbcf589316cfc075d
MiGaNg <miguelangelyv1@gmail.com>
2024-12-11 18:15:52 -0500

    JournalApp  - MaterialUI - Estructura y Diseño

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,8 @@
 
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NoteView } from '../views'
+import { NoteView, NothingSelectedView } from '../views'
+import { AddOutlined } from '@mui/icons-material'
 
 
 export const JournalPage = () => {
@@ -8,8 +10,22 @@ export const JournalPage = () => {
     <>
     <JournalLayout>
         {/* <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab doloribus explicabo iusto omnis saepe dolorum ex aliquam? Ipsa earum deleniti quaerat esse nesciunt error vel deserunt quos quasi, ratione maiores?</Typography> */}
-        {/* <NothingSelectedView/> */}
-        <NoteView/>
+        <NothingSelectedView/>
+        {/* <NoteView/> */}
+
+        <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor:'error.main',
+          ':hover':{ backgroundColor:'error.main', opacity:0.8},
+          position:'fixed',
+          right:50,
+          bottom:50
+        }}
+        >
+          <AddOutlined sx={{fonsize:20}}/>
+        </IconButton>
     </JournalLayout>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { JournalApp } from './JournalApp.jsx'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <JournalApp />
+    <Provider store={store}>
+      <JournalApp />
+    </Provider>
   </StrictMode>,
 )
