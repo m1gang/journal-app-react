74c17a9922e6008fec92abb4ecdef95a5018d274
MiGaNg <miguelangelyv1@gmail.com>
2024-12-17 15:56:59 -0500

    Creamos la funcion Google SignIn de Firebase para capturar datos

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { signInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials } from "./"
 
 export const checkingAuthentication = (email,password) => {
@@ -10,6 +11,8 @@ export const checkingAuthentication = (email,password) => {
 export const startGoogleSignIn = () => {
     return async (dispatch) => {
         dispatch(checkingCredentials());
+        const result = await signInWithGoogle();
+        console.log(result);
         
     }
 }
