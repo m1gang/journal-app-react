60958104849ca1334c0ebde85d2ef0702a1af9ef
MiGaNg <miguelangelyv1@gmail.com>
2024-12-20 17:53:27 -0500

    Mostramos el mensaje de error de atenticación

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,38 +1,40 @@
-import { Button, Grid, TextField, Typography, Link } from "@mui/material"
+import { Button, Grid, TextField, Typography, Link, Alert, Grid2 } from "@mui/material"
 import { AuthLayout } from "../layout/AuthLayout"
 import { Link as RouterLink } from "react-router"
 import { useForm } from "../../hooks/useForm";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { startCreatingUserWithEmailPassword } from "../../store/auth";
 
 const formData = {
   displayName: 'migatoSEnsei',
-  email:'miguelangelyv2@gmail.com',
+  email: 'miguelangelyv2@gmail.com',
   password: '123456',
 }
 
 const formValidations = {
-  email: [(value)=> value.includes('@'),'El correo debe tener una @'],
-  password: [(value)=> value.length >= 6,'La contraseña debe tener más de 6 letras'],
-  displayName: [(value)=> value.length >= 1,'El nombre es obligatorio'],
+  email: [(value) => value.includes('@'), 'El correo debe tener una @'],
+  password: [(value) => value.length >= 6, 'La contraseña debe tener más de 6 letras'],
+  displayName: [(value) => value.length >= 1, 'El nombre es obligatorio'],
 }
 
 export const RegisterPage = () => {
+  const { status, errorMessage } = useSelector(state => state.auth);
+  const isCheckingAuthentication = useMemo(() => status == 'checking', [status]);
 
   const dispatch = useDispatch();
   const [formSubmitted, setFormSubmitted] = useState(false);
 
   const {
-    formState, displayName, email, password, onInputChange, 
+    formState, displayName, email, password, onInputChange,
     formValid, displayNameValid, emailValid, passwordValid
-  } = useForm(formData,formValidations);
+  } = useForm(formData, formValidations);
 
 
   const onSubmit = (e) => {
     e.preventDefault();
     setFormSubmitted(true);
-    if(!formValid) return;
+    if (!formValid) return;
     console.log(formState)
     dispatch(startCreatingUserWithEmailPassword(formState))
   }
@@ -52,8 +54,8 @@ export const RegisterPage = () => {
               name='displayName'
               value={displayName}
               onChange={onInputChange}
-              error={ !!displayNameValid && formSubmitted }
-              helperText={ displayNameValid }
+              error={!!displayNameValid && formSubmitted}
+              helperText={displayNameValid}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -65,8 +67,8 @@ export const RegisterPage = () => {
               name='email'
               value={email}
               onChange={onInputChange}
-              error={ !!emailValid && formSubmitted }
-              helperText={ emailValid }
+              error={!!emailValid && formSubmitted}
+              helperText={emailValid}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -78,26 +80,41 @@ export const RegisterPage = () => {
               name='password'
               value={password}
               onChange={onInputChange}
-              error={ !!passwordValid  && formSubmitted}
-              helperText={ passwordValid }
+              error={!!passwordValid && formSubmitted}
+              helperText={passwordValid}
             />
           </Grid>
 
 
           <Grid container spacing={2} sx={{ mt: 2 }}>
+
+            <Grid2 item xs={12} sm={12}
+              display={!!errorMessage ? 'flex' : 'none'}
+              sx={{
+                border: '1px solid red',
+                borderRadius: '5px',
+                width:'100%',
+                marginLeft:'16px'
+              }}
+            >
+              <Alert severity="error" sx={{ width: '100%' }}>{errorMessage}</Alert>
+            </Grid2>
+
+
             <Grid item xs={12} sm={12}>
-              <Button 
-              type="submit"
-              variant="contained" 
-              fullWidth>
+              <Button
+                disabled={isCheckingAuthentication}
+                type="submit"
+                variant="contained"
+                fullWidth>
                 Crear Cuenta
               </Button>
             </Grid>
-            
+
           </Grid>
 
-          <Grid container direction='row' justifyContent='end' sx={{mt:2}}>
-            <Typography sx={{mr:1}}>¿Ya tienes cuenta?</Typography>
+          <Grid container direction='row' justifyContent='end' sx={{ mt: 2 }}>
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
             <Link component={RouterLink} color='inherit' to='/auth/login'>
               Ingresar
             </Link>
