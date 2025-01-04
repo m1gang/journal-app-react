4c020fd2f6ebbd45a6c41d44b1f804e7b99a5906
MiGaNg <miguelangelyv1@gmail.com>
2025-01-03 20:13:39 -0500

    Agregamos animaciones a la aplicación

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -42,8 +42,8 @@ export const RegisterPage = () => {
 
   return (
     <AuthLayout title="Register">
-      <h1>Form Valid: {formValid ? 'Valido' : 'Incorrecto'}</h1>
-      <form onSubmit={onSubmit}>
+      
+      <form onSubmit={onSubmit} className="animate__animated animate__fadeIn animate__faster">
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
