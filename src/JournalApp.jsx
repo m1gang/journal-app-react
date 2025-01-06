d3c2ecd8be839b3372e80a0612f05ece4ca3f22c
MiGaNg <miguelangelyv1@gmail.com>
2025-01-06 17:10:37 -0500

    Introducción a Redux y autenticación en firebase

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router } from "react-router"
 import { AppRouter } from "./router/AppRouter"
 import { AppTheme } from "./theme"
-
-
+//17 - JournalApp  - MaterialUI - Estructura y Diseño
+//19 - Introducción a Redux y autenticación en Firebase
 export const JournalApp = () => {
   return (
     <>
