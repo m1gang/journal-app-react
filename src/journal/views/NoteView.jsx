bded6ea2ca72ebead942d6df0a098c503afd0d3c
MiGaNg <miguelangelyv1@gmail.com>
2025-01-15 16:46:09 -0500

    Seleccionamos archivos desde React

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,9 +1,9 @@
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
+import { SaveOutlined, UploadFile} from "@mui/icons-material"
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
 import { useForm } from "../../hooks/useForm"
 import { useDispatch, useSelector } from "react-redux"
-import { useEffect, useMemo } from "react"
+import { useEffect, useMemo, useRef } from "react"
 import { setActiveNote } from "../../store/journal/journalSlice"
 import { startSaveNote } from "../../store/journal/thunks"
 import { Toaster } from "sonner"
@@ -21,6 +21,8 @@ export const NoteView = () => {
         return newDate.toUTCString()
     }, [date])
 
+    const fileInputRef = useRef();
+
     useEffect(() => {
         dispatch(setActiveNote(formState));
 
@@ -36,6 +38,13 @@ export const NoteView = () => {
         dispatch(startSaveNote());
     }
 
+    const onFileInputChange = ({target}) => {
+        if (target.files ===0) return;
+        console.log('Subiendo Archivos');
+        
+        
+    }
+
     return (
         <Grid
             container
@@ -48,6 +57,20 @@ export const NoteView = () => {
                 <Typography fontSize={39} fontWeight='light'>{dateString}</Typography>
             </Grid>
             <Grid item>
+
+                <input type="file"
+                multiple
+                ref={fileInputRef}
+                onChange={onFileInputChange}
+                style={{display:'none' }} />
+                <IconButton>
+                    <UploadFile
+                        color="primary"
+                        disabled={isSaving}
+                        onClick={()=> fileInputRef.current.click()}
+                    />
+                </IconButton>
+
                 <Button 
                 disabled={isSaving}
                 onClick={ onSaveNote }
