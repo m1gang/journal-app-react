fba42a94f877220ef5f24359e7ce1b1e7fc24c8d
MiGaNg <miguelangelyv1@gmail.com>
2025-03-19 17:07:45 -0500

    Creamos la base de datos de testing

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,13 +7,25 @@ import firebase from "firebase/compat/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+// Dev Prod
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA8Wgb0bGiI8kc21iWeDZ0XJ-iXMhiHO7Q",
+//   authDomain: "react-cursos-647ac.firebaseapp.com",
+//   projectId: "react-cursos-647ac",
+//   storageBucket: "react-cursos-647ac.firebasestorage.app",
+//   messagingSenderId: "635607623928",
+//   appId: "1:635607623928:web:bb82b147787283b67cf30d"
+// };
+
+//Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyA8Wgb0bGiI8kc21iWeDZ0XJ-iXMhiHO7Q",
-  authDomain: "react-cursos-647ac.firebaseapp.com",
-  projectId: "react-cursos-647ac",
-  storageBucket: "react-cursos-647ac.firebasestorage.app",
-  messagingSenderId: "635607623928",
-  appId: "1:635607623928:web:bb82b147787283b67cf30d"
+  apiKey: "AIzaSyAYXtYnTN-GAt-t7vF093BXM00ku8GMX2c",
+  authDomain: "react-app-j.firebaseapp.com",
+  projectId: "react-app-j",
+  storageBucket: "react-app-j.firebasestorage.app",
+  messagingSenderId: "807219248799",
+  appId: "1:807219248799:web:9ffc4810081b5e4213acaa"
 };
 
 // Initialize Firebase
