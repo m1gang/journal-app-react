daa4de8d0d131b331c2b2c4761a92cf2f8e5d936
MiGaNg <miguelangelyv1@gmail.com>
2025-03-19 18:38:49 -0500

    Prueba completa sobre inserción y limpieza de notas

--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,4 +1,6 @@
-import { startNewNote } from "../../../src/store/journal";
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../../src/firebase/config";
+import { addNewEmptyNote, savingNewNote, setActiveNote, startNewNote } from "../../../src/store/journal";
 
 describe('Pruebas en Journal Thunks', () => { 
 
@@ -14,8 +16,33 @@ describe('Pruebas en Journal Thunks', () => {
 
         await startNewNote()(dispatch,getState);
 
+        expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+        expect(dispatch).toHaveBeenCalledWith(addNewEmptyNote({
+            body:'',
+            title:'',
+            id: expect.any(String),
+            date: expect.any(Number),
+            imageUrls: expect.any(Array),
+        }));
+        expect(dispatch).toHaveBeenCalledWith(setActiveNote({
+            body:'',
+            title:'',
+            id: expect.any(String),
+            date: expect.any(Number),
+            imageUrls: expect.any(Array),
+        }));
 
-    })
+
+        //Borrar de Firebase
+        const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+        const docs = await getDocs(collectionRef);
+
+        const deletePromises = [];
+        docs.forEach(doc => deletePromises.push(deleteDoc(doc.ref)));
+        await Promise.all(deletePromises);
+           
+      
+    },10000);
     
 
  })
